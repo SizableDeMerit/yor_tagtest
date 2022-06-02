bc1f49a3593cd841904e4f7fffb317a7c0a7145c
SizableDeMerit <sized-demerit-0u@icloud.com>
2022-06-02 17:39:43 +0000

    testing soup to nuts

--- main.tf
+++ main.tf
@@ -19,21 +19,7 @@ resource "aws_accessanalyzer_analyzer" "account_level" {
   #id            = "ConsoleAnalyzer-5f9facca-fc40-4973-8cd2-9526136ad4be"
   #tags           = local.common_tags
   type = "ACCOUNT"
-
-  # tags = merge(local.common_tags, {
-  #   yor_trace = "8c9fceac-da3b-4c9d-9991-db61b665ea00"
-  # })
-
-  tags = {
-    yor_trace            = "cf314b13-6c92-48f1-9a5a-8e29eec0a327"
-    git_commit           = "18ae04308c86227973cc20a889811590c9d8c208"
-    git_file             = "main.tf"
-    git_last_modified_at = "2022-06-02 15:23:29"
-    git_last_modified_by = "sized-demerit-0u@icloud.com"
-    git_modifiers        = "102994153+SizableDeMerit/sized-demerit-0u"
-    git_org              = "SizableDeMerit"
-    git_repo             = "yor_tagtest"
-  }
+  tags          = local.common_tags
 }
 
 
@@ -44,14 +30,5 @@ resource "aws_accessanalyzer_analyzer" "org_level" {
   #id            = "ConsoleAnalyzer-5f9facca-fc40-4973-8cd2-9526136ad4be"
   #tags          = local.common_tags
   type = "ORGANIZATION"
-  tags = {
-    yor_trace            = "884c1fce-95de-4c0f-9ea0-24ab71cde426"
-    git_commit           = "9d431a68b25e187fd937e43c1a76b181d96a4d95"
-    git_file             = "main.tf"
-    git_last_modified_at = "2022-05-26 20:38:09"
-    git_last_modified_by = "102994153+SizableDeMerit@users.noreply.github.com"
-    git_modifiers        = "102994153+SizableDeMerit"
-    git_org              = "SizableDeMerit"
-    git_repo             = "yor_tagtest"
-  }
+  tags          = local.common_tags
 }
